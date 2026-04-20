c39e254364e01a4d5bbc1f7e4747b53f2dca06ca
sumit-raghav-06 <sumitraghavxc@gmail.com>
2026-04-20 11:01:53 +0530

    04_Remove_duplicate_from_sorted_array.cpp

---
+++ Arryas/04_Remove_duplicate_from_sorted_array.cpp
@@ -0,0 +1,15 @@
+// Platform: Leetcode
+// Difficulty: Easy
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+        for(int j = 1; j<nums.size(); j++){
+            if(nums[j] != nums[j-1]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+};
