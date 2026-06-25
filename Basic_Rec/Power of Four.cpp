94ba0c12bf62ca42486eb500793a1948d982d25a
Sumit <sumitraghavxc@gmail.com>
2026-06-25 20:00:29 +0530

    Power of Four.cpp

--- Basic_Rec/Power of Four.cpp
+++ Basic_Rec/Power of Four.cpp
@@ -1,8 +1,8 @@
 //Leetcode // Easy
 class Solution {
 public:
-    bool isPowerOfThree(int n) {
-        if(n<=0 || n % 3 != 0) return n ==1;
-        return isPowerOfThree(n/3);
+    bool isPowerOfFour(int n) {
+        if(n <=1 || n % 4 !=0) return n==1;
+        return isPowerOfFour(n/4);
     }
 };
