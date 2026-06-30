89dbfbd63d42104f03c7db97c9690b7e9740a829
Sumit <sumitraghavxc@gmail.com>
2026-06-30 23:48:20 +0530

    Create search-in-rotated-sorted-array-ii.cpp

---
+++ Binary search/search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,37 @@
+// leetcode plat;
+
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size()-1;
+
+        while(left <= right){
+            int mid = left+(right - left)/2;
+            if(nums[mid] == target){
+                return true;
+            }
+            if(nums[left] == nums[mid] && nums[mid] == nums[right]){
+                left++;
+                right--;
+                continue;
+
+            }
+            if(nums[left] <= nums[mid]){
+                if(nums[left]<= target && target <= nums[mid]){
+                    right = mid-1;
+                } else{
+                    left = mid+1;
+                }
+            } else{
+                if(nums[mid] <= target && nums[right] >= target) {
+                    left = mid+1;
+                }else{
+                    right = mid-1;
+                }
+            }
+        }
+        return false;
+    }
+};
